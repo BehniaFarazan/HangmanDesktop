142f0bc6dfe0878ae7f9d7dcfd9252426765f36b
BehniaFarazan <b.farazan@gmail.com>
2018-11-17 09:28:46 +0100

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -384,7 +384,7 @@ namespace HangmanGameDesktop
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
