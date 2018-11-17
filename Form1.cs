a4f43c97f3a5caed878f5fb95631a165763ccc7d
BehniaFarazan <b.farazan@gmail.com>
2018-11-17 10:14:21 +0100

    commit

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ namespace HangmanGameDesktop
 {
     public partial class Form1 : Form
     {
+     
         public Form1()
         {
             InitializeComponent();
@@ -22,10 +23,13 @@ namespace HangmanGameDesktop
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBoxShowWord.Text = Mylogic.getTheWord();
+            textBoxShowHiddenWord.Text = Mylogic.makeWordHidden();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
+        {
+        }
+            private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
---
+++ Mylogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanGameDesktop
+{
+   
+    class Mylogic
+    {
+       private String  theWord;
+       private static String xWord;
+
+
+
+        public static String getTheWord() {
+            return "TR";
+        }
+
+        public static String getThexWord()
+        {
+            return "";
+        }
+        public static String makeWordHidden()
+        {
+            xWord = getThexWord();
+            for (int i =0; i< getTheWord().Length ; i++)
+            {
+                xWord = xWord + "#";
+            }
+
+            return xWord;
+        }
+        public void setTheWord(String theWord) {
+            
+        }
+
+        public void changeGalgePic() {
+
+        }
+    }
+}
